86f9a7d457d6c4394c4e20e5f777635dcb53a94b
Federico Marmo <federico.marmo.1990@gmail.com>
2024-03-26 21:02:42 -0300

    changeg folder structure and reorganized files accordingly

--- src/renderer/compontents/app/app.tsx
+++ src/renderer/compontents/app/app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
+import './app.css'
 
 const root = createRoot(document.body);
 
-root.render(<h2>Hello from React!!</h2>);
+root.render(<h2>Hello from React!! ♥</h2>);
